09eeb9c14e6bc5ecb1fc1597eaf16a4254f62be6
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-19 23:09:13 +0600

    Added: Product Dto added

---
+++ InventoryManagement.Application/DTOs/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Application.DTOs
+{
+    internal class ProductDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
