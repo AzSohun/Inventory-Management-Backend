5ea788d05b4456c7ba0e41ae061b0d01be788ef4
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-20 01:16:07 +0600

    Registered ConnectionString

--- InventoryManagement.Api/Program.cs
+++ InventoryManagement.Api/Program.cs
@@ -1,8 +1,19 @@
+using InventoryManagement.Infastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ProductDbContext>(options => {
+
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultString"));
+
+});
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
