bdab73b94786939521d66216671f68cff4b552cd
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-19 23:07:29 +0600

    Added: Create Product Dto

---
+++ InventoryManagement.Application/DTOs/CreateProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Application.DTOs
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
