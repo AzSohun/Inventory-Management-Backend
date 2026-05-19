2159c631922e96deb776654e28f7be3418afdb3c
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-19 21:45:08 +0600

    Added: product Model

---
+++ InventoryManagement.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Domain.Entities
+{
+    internal class Product
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } 
+
+    }
+}
