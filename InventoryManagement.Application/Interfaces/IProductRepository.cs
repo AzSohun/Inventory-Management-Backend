b25d70c54877954acc474608a0b0a000d680d220
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-19 22:53:07 +0600

    Added: Product Repository

--- InventoryManagement.Application/Interfaces/IProductRepository.cs
+++ InventoryManagement.Application/Interfaces/IProductRepository.cs
@@ -10,7 +10,7 @@ namespace InventoryManagement.Application.Interfaces
         Task<IEnumerable<Product>> GetAllAsync();
         Task<Product?> GetByIdAsync(Guid id);
         Task CreateAsync(Product product);
-        Task UpdateAsync(Guid id, Product product);
+        Task UpdateAsync(Product product);
         Task DeleteAsync(Guid id);
     }
 }
---
+++ InventoryManagement.Infastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using InventoryManagement.Application.Interfaces;
+using InventoryManagement.Domain.Entities;
+using InventoryManagement.Infastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Infastructure.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+
+        private readonly ProductDbContext _context;
+
+        public ProductRepository(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(Guid id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+
+        public async Task CreateAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if(product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
