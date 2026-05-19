3c840666778c4cd5488de235248b101c3027899f
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-19 22:30:14 +0600

    Done: ProductDbContext setup done

---
+++ InventoryManagement.Infastructure/Data/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using InventoryManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Infastructure.Data
+{
+    public class ProductDbContext: DbContext
+    {
+
+        public ProductDbContext(DbContextOptions<ProductDbContext> options): base(options)
+        {
+            
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
